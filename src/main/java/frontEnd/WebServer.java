d210741209c79bfef6bd8f2ecc978ccaf7cf0065
AlexisRizo <tsuyuki@live.com>
2023-06-25 09:39:18 -0600

    Cambios en indentación y simplificación y limpieza de código. Punto previo a la funcionalidad de registro de servidores.

--- src/main/java/frontEnd/WebServer.java
+++ src/main/java/frontEnd/WebServer.java
@@ -2,10 +2,8 @@ package frontEnd;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
-import frontEnd.auxiliares.Ordena;
 import frontEnd.auxiliares.Text;
 import frontEnd.networking.Aggregator;
 
@@ -18,168 +16,150 @@ import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class WebServer {
-   
-    private static final String STATUS_ENDPOINT = "/status";
-    private static final String HOME_PAGE_ENDPOINT = "/";
-    private static final String HOME_PAGE_UI_ASSETS_BASE_DIR = "/ui_assets/";
-    private static final String ENDPOINT_PROCESS = "/procesar_datos";
-    private static final String WORKER_ADDRESS_1 = "http://127.0.0.1:8081/analisis_libros";
-    private static final String WORKER_ADDRESS_2 = "http://127.0.0.1:8082/analisis_libros";
-    private static final String WORKER_ADDRESS_3 = "http://127.0.0.1:8083/analisis_libros";
-    private static final int NUM_BOOKS = 46;
-
-    private final int port; 
-    private HttpServer server; 
-    private final ObjectMapper objectMapper;
-
-    public WebServer(int port) {
-        this.port = port;
-        this.objectMapper = new ObjectMapper();
-        this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
-
-    public void startServer() {
-        try {
-            this.server = HttpServer.create(new InetSocketAddress(port), 0);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        HttpContext statusContext = server.createContext(STATUS_ENDPOINT); 
-        HttpContext taskContext = server.createContext(ENDPOINT_PROCESS);
-        HttpContext homePageContext = server.createContext(HOME_PAGE_ENDPOINT);
-        statusContext.setHandler(this::handleStatusCheckRequest);
-        taskContext.setHandler(this::handleTaskRequest);
-        homePageContext.setHandler(this::handleRequestForAsset);
-
-        server.setExecutor(Executors.newFixedThreadPool(8));
-        server.start();
-    }
-
-    private void handleRequestForAsset(HttpExchange exchange) throws IOException {
-        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
-            exchange.close();
-            return;
-        }
-
-        byte[] response;
-
-        String asset = exchange.getRequestURI().getPath(); 
-
-        if (asset.equals(HOME_PAGE_ENDPOINT)) { 
-            response = readUiAsset(HOME_PAGE_UI_ASSETS_BASE_DIR + "index.html");
-        } else {
-            response = readUiAsset(asset); 
-        }
-        addContentType(asset, exchange);
-        sendResponse(response, exchange);
-    }
-
-    private byte[] readUiAsset(String asset) throws IOException {
-        InputStream assetStream = getClass().getResourceAsStream(asset);
-
-        if (assetStream == null) {
-            return new byte[]{};
-        }
-        return assetStream.readAllBytes(); 
-    }
-
-    private static void addContentType(String asset, HttpExchange exchange) {
-
-        String contentType = "text/html";  
-        if (asset.endsWith("js")) {
-            contentType = "text/javascript";
-        } else if (asset.endsWith("css")) {
-            contentType = "text/css";
-        }
-        else if (asset.endsWith("ico")) {
-            contentType = "image/x-icon";
-        }
-        exchange.getResponseHeaders().add("Content-Type", contentType);
-    }
-
-    private void handleTaskRequest(HttpExchange exchange) throws IOException {
-        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) { 
-            exchange.close();
-            return;
-        }
-
-        try {
-            FrontendSearchRequest frontendSearchRequest = objectMapper.readValue(exchange.getRequestBody().readAllBytes(), FrontendSearchRequest.class); 
-            String string = frontendSearchRequest.getSearchQuery();
-            int division = NUM_BOOKS/3;
-            int inicial = 0;
-            String task1 = new String(inicial+","+(division-1)+","+string);
-            String task2 = new String(division+","+((division*2)-1)+","+string);
-            String task3 = new String((division*2)+","+(NUM_BOOKS-1)+","+string);
-            System.out.println("Los intervalos son:");
-            System.out.println("Intervalo 1: "+task1);
-            System.out.println("Intervalo 2: "+task2);
-            System.out.println("Intervalo 3: "+task3);
-            Aggregator aggregator = new Aggregator();
-            Map<String, List<Double>> Libros = aggregator.sendTasksToWorkers(Arrays.asList(WORKER_ADDRESS_1,WORKER_ADDRESS_2,WORKER_ADDRESS_3),Arrays.asList(task1,task2,task3));
-            int n_frase = string.split(" ").length;
-            double[] nt = new double[n_frase];
-            for(double d: nt)
-                d = 0.0;
-            //Numero de apariciones en libros
-            for (Map.Entry<String, List<Double>> e : Libros.entrySet()){
-                int contador = 0;
-                for(Double d : e.getValue()){
-                    if(d>0.0)
-                        nt[contador] += 1;
-                    contador++;
-                }
-            }
-            //Calculo de la tercer propuesta
-            List<Text> books = new ArrayList<>();
-            for (Map.Entry<String, List<Double>> e : Libros.entrySet()){
-                int contador = 0;
-                double fitness = 0;
-                for(Double d : e.getValue()){
-                    fitness += Math.log10(Libros.size()/nt[contador])*d;
-                }
-                books.add(new Text(e.getKey(),fitness));
-            }
-
-            Collections.sort(books,new Ordena());
-            Collections.reverse(books);
-
-            List<Text> Resultados = books.stream().filter(e -> e.fitness != 0).collect(Collectors.toList());
-
-            String cadena = " ";
-            for(Text t: Resultados)
-                cadena += t.toString()+"\n";
-
-            StringTokenizer st = new StringTokenizer(cadena);
-            FrontendSearchResponse frontendSearchResponse = new FrontendSearchResponse(cadena, st.countTokens());
-            byte[] responseBytes = objectMapper.writeValueAsBytes(frontendSearchResponse);
-            sendResponse(responseBytes, exchange);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-    }
-
-    private void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
-        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
-            exchange.close();
-            return;
-        }
-
-        String responseMessage = "El servidor está vivo\n";
-        sendResponse(responseMessage.getBytes(), exchange);
-    }
-
-    private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
-        exchange.sendResponseHeaders(200, responseBytes.length);
-        OutputStream outputStream = exchange.getResponseBody();
-        outputStream.write(responseBytes);
-        outputStream.flush();
-        outputStream.close();
-    }
+	private static final String STATUS_ENDPOINT = "/status";
+	private static final String HOME_PAGE_ENDPOINT = "/";
+	private static final String PROCESS_ENDPOINT = "/procesar_datos";
+	private static final String REGISTER_ENDPOINT = "/registrar";
+	
+	private static final String WORKER_ADDRESS_1 = "http://127.0.0.1:8081/analisis_libros";
+	private static final String WORKER_ADDRESS_2 = "http://127.0.0.1:8082/analisis_libros";
+	private static final String WORKER_ADDRESS_3 = "http://127.0.0.1:8083/analisis_libros";
+	private static final String HOME_PAGE_UI_ASSETS_BASE_DIR = "/ui_assets/";
+	private static final int NUM_BOOKS = 46;
+	private static final List<String> servidores = new ArrayList<>();
+	
+	private static final ObjectMapper objectMapper = new ObjectMapper()
+		.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	
+	public static void main(String[] args) throws IOException {
+		int port = args.length == 1 ? Integer.parseInt(args[0]) : 3000;
+		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+		
+		server.createContext(HOME_PAGE_ENDPOINT).setHandler(WebServer::handleRequestForAsset);
+		server.createContext(PROCESS_ENDPOINT).setHandler(WebServer::handleTaskRequest);
+		server.createContext(STATUS_ENDPOINT).setHandler(WebServer::handleStatusCheckRequest);
+		server.createContext(REGISTER_ENDPOINT).setHandler(WebServer::handleRegisterRequest);
+		
+		server.setExecutor(Executors.newFixedThreadPool(8));
+		server.start();
+		
+		System.out.println("Servidor escuchando en el puerto: " + port);
+	}
+	
+	private static void handleRequestForAsset(HttpExchange exchange) throws IOException {
+		if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+			exchange.close();
+			return;
+		}
+		
+		String asset = exchange.getRequestURI().getPath();
+		
+		byte[] response = asset.equals(HOME_PAGE_ENDPOINT) ?
+				readUiAsset(HOME_PAGE_UI_ASSETS_BASE_DIR + "index.html")
+			: readUiAsset(asset);
+		addContentType(asset, exchange);
+		sendResponse(response, exchange);
+	}
+	
+	private static void handleTaskRequest(HttpExchange exchange) {
+		if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+			exchange.close();
+			return;
+		}
+		
+		try {
+			FrontendSearchRequest frontendSearchRequest = objectMapper.readValue(
+				exchange.getRequestBody().readAllBytes(),
+				FrontendSearchRequest.class
+			);
+			String query = frontendSearchRequest.getSearchQuery();
+			int division = NUM_BOOKS / 3;
+			int inicial = 0;
+			
+			String task1 = inicial + "," + (division - 1) + "," + query;
+			String task2 = division + "," + ((division * 2) - 1) + "," + query;
+			String task3 = (division * 2) + "," + (NUM_BOOKS - 1) + "," + query;
+			System.out.println("Los intervalos son:");
+			System.out.println("Intervalo 1: "+task1);
+			System.out.println("Intervalo 2: "+task2);
+			System.out.println("Intervalo 3: "+task3);
+			Map<String, List<Double>> libros = new Aggregator().sendTasksToWorkers(
+				Arrays.asList(WORKER_ADDRESS_1,WORKER_ADDRESS_2,WORKER_ADDRESS_3),
+				Arrays.asList(task1,task2,task3)
+			);
+			
+			int n_frase = query.split(" ").length;
+			double[] nt = new double[n_frase];
+			
+			//Numero de apariciones en libros
+			for (Map.Entry<String, List<Double>> e : libros.entrySet()){
+				int contador = 0;
+				for (Double d : e.getValue()) {
+					if (d > 0.0)
+						nt[contador] += 1;
+					contador++;
+				}
+			}
+			//Calculo de la tercer propuesta
+			List<Text> books = new ArrayList<>();
+			for (Map.Entry<String, List<Double>> e : libros.entrySet()){
+				int contador = 0;
+				double fitness = 0;
+				for(Double d : e.getValue()){
+					fitness += Math.log10(libros.size()/nt[contador])*d;
+				}
+				books.add(new Text(e.getKey(),fitness));
+			}
+			
+			books.sort(Comparator.comparingDouble((Text text) -> text.fitness).reversed());
+			
+			List<Text> resultados = books.stream().filter(e -> e.fitness != 0).collect(Collectors.toList());
+			
+			String cadena = " ";
+			for(Text t: resultados)
+				cadena += t.toString()+"\n";
+			
+			StringTokenizer st = new StringTokenizer(cadena);
+			FrontendSearchResponse frontendSearchResponse = new FrontendSearchResponse(cadena, st.countTokens());
+			byte[] responseBytes = objectMapper.writeValueAsBytes(frontendSearchResponse);
+			sendResponse(responseBytes, exchange);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
+		String responseMessage = "El servidor está vivo\n";
+		sendResponse(responseMessage.getBytes(), exchange);
+	}
+	
+	private static void handleRegisterRequest(HttpExchange exchange) {
+	
+	}
+	
+	private static byte[] readUiAsset(String asset) {
+		try (InputStream assetStream = WebServer.class.getResourceAsStream(asset)) {
+			return assetStream == null ? new byte[]{} : assetStream.readAllBytes();
+		} catch (IOException e) {
+			return new byte[]{};
+		}
+	}
+	
+	private static void addContentType(String asset, HttpExchange exchange) {
+		exchange.getResponseHeaders().add(
+			"Content-Type",
+			asset.endsWith("js") ? "text/javascript"
+				: asset.endsWith("css") ? "text/css"
+				: "text/html"
+		);
+	}
+	
+	private static void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
+		exchange.sendResponseHeaders(200, responseBytes.length);
+		try (OutputStream outputStream = exchange.getResponseBody()) {
+			outputStream.write(responseBytes);
+		}
+	}
 }
 
 
