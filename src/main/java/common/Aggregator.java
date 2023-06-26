706129d8d2dc181143449995ca5c9a0bd538644e
Denzel011224 <82571352+Denzel011224@users.noreply.github.com>
2023-06-25 21:45:23 -0600

    Correccion futures

--- src/main/java/common/Aggregator.java
+++ src/main/java/common/Aggregator.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -17,13 +18,21 @@ public class Aggregator {
 		}
 		boolean bandera = true;
 		while (bandera) {
-			if (futures[0].isDone() && futures[1].isDone() && futures[2].isDone())
+			int contador = 0;
+			for(CompletableFuture<byte[]> future : futures){
+				if(future.isDone())
+					contador++;
+			}
+			if (futures.length==contador)
 				bandera = false;
 		}
 		Map<String, List<Double>> libros = new HashMap<>();
 		for (CompletableFuture<byte[]> future : futures) {
 			libros.putAll((Map<String, List<Double>>) SerializationUtils.deserialize(future.join()));
 		}
+
+		/*for (Map.Entry<String,List<Double>> entry : libros.entrySet())
+			System.out.println(entry.getKey()+"  "+Arrays.toString(entry.getValue().toArray()));*/
 		return libros;
 	}
 	
